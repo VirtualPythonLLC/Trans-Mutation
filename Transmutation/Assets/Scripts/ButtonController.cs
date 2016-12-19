c141edf93793a3f80df5e3e77037e5872b219c07
Luciano Putruele <lputruele@gmail.com>
2016-12-19 14:57:24 -0200

    Updated project to Unity 5.5

---
+++ Transmutation/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonController : RaycastController {
+		
+
+	public override void Start() {
+		base.Start ();
+		UpdateRaycastOrigins ();
+	}
+	
+	public bool CanBePressed() {
+		float rayLength = skinWidth;
+
+		if (1 < skinWidth) {
+			rayLength = 2*skinWidth;
+		}
+
+		for (int i = 0; i < horizontalRayCount; i ++) {
+			//check Left collisions
+			Vector2 rayOrigin = raycastOrigins.bottomLeft;
+			Debug.Log (rayOrigin.ToString());
+			rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+			RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.left, rayLength, collisionMask);
+
+			Debug.DrawRay(rayOrigin, Vector2.left * 0.2f,Color.red);
+			
+			if (hit) {
+				if (hit.collider.tag == "Player"){
+					return true;
+				}
+			}
+			//check Right collisions
+			rayOrigin = raycastOrigins.bottomRight;
+			rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+			hit = Physics2D.Raycast(rayOrigin, Vector2.right, rayLength, collisionMask);
+			
+			Debug.DrawRay(rayOrigin, Vector2.right * 0.2f,Color.red);
+			
+			if (hit) {
+				if (hit.collider.tag == "Player"){
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}	
+}
---
+++ Transmutation/Assets/Scripts/LevelButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (ButtonController))]
+public class LevelButton : MonoBehaviour {
+
+	ButtonController controller;
+	public GameObject triggeredObject;
+	bool pressed;
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<ButtonController> ();
+	}
+	
+	// Update is called once per frame
+	void Update(){
+		if (Input.GetAxisRaw("Fire1") > 0 && !pressed && controller.CanBePressed()){
+			Press();
+			pressed = true;
+		}
+		if (Input.GetAxisRaw("Fire1") == 0)
+			pressed = false;
+	}
+
+	public void Press() {
+		if (triggeredObject.activeSelf)
+			triggeredObject.SetActive(false);
+		else
+			triggeredObject.SetActive(true);
+	}
+}
